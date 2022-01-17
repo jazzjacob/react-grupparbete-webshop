924b886e909d0c8558590dd46a764219d9efa47b
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-01-17 15:40:32 +0100

    Add files via upload

--- src/components/views/Shop.js
+++ src/components/views/Shop.js
@@ -1,8 +1,11 @@
+import ProductList from '../general/ProductList';
+
 const Shop = () => {
   return (
     <main>
       <h1>Goa gubbar till salu</h1>
       <h2>Billigt billigt</h2>
+      <ProductList />
     </main>
   );
 }
