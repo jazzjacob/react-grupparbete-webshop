8cd832ad4a0e4b1f4dcd5967897fda4ab88a1859
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-01-18 11:51:32 +0100

    More functionality

--- src/components/general/ShippingForm.js
+++ src/components/general/ShippingForm.js
@@ -1,4 +1,22 @@
+import { UserContext } from '../../context/UserContext';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const ShippingForm = () => {
+
+  const [orderMade, setOrderMade] = useState(false);
+  const [shippingInfo, setShippingInfo] = useState({
+    firstName: '',
+    lastName: '',
+    phone: '',
+    address: '',
+    postcode: '',
+    town: '',
+  });
+
+  const navigate = useNavigate();
+
+
   const formStyle = {
     display: 'flex',
     flexDirection: 'column',
@@ -11,35 +29,75 @@ const ShippingForm = () => {
   }
 
   const formButtonStyle = {
-    height: '30px',
+    height: '40px',
     backgroundColor: 'gray',
     color: 'white'
   }
 
+  const makeOrder = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(shippingInfo));
+    setOrderMade(true);
+  }
+
+  const handleInput = (e) => {
+    setShippingInfo({...shippingInfo, [e.target.name]: e.target.value});
+  }
+  console.log(shippingInfo);
+
   return (
-    <form style={formStyle}>
-      <h3>Shipping address</h3>
-
-      <label for='firstName'>First name</label>
-      <input name='firstName' style={formInputStyle} />
-
-      <label for='lastName'>Last name</label>
-      <input name='lastName' style={formInputStyle} />
-
-      <label for='phone'>Phone number</label>
-      <input name='lastName' style={formInputStyle} />
-
-      <label for='address'>Address</label>
-      <input name='address' style={formInputStyle} />
-
-      <label for='postcode'>Postcode</label>
-      <input name='postcode' style={formInputStyle} />
-
-      <label for='town'>Town/City</label>
-      <input name='town' style={formInputStyle} />
-
-      <button style={formButtonStyle}>Order</button>
-    </form>
+    <>
+      {!orderMade ? (
+         <form style={formStyle} onSubmit={makeOrder}>
+         <h3>Shipping address</h3>
+   
+         <label>First name</label>
+         <input
+           name='firstName'
+           style={formInputStyle}
+           onChange={handleInput} />
+   
+         <label>Last name</label>
+         <input
+           name='lastName'
+           style={formInputStyle}
+           onChange={handleInput} />
+   
+         <label>Phone number</label>
+         <input
+           name='phone'
+           style={formInputStyle}
+           onChange={handleInput}
+           type='phone'/>
+   
+         <label>Address</label>
+         <input
+           name='address'
+           style={formInputStyle}
+           onChange={handleInput} />
+   
+         <label>Postcode</label>
+         <input
+           name='postcode'
+           style={formInputStyle}
+           onChange={handleInput}/>
+   
+         <label>Town/City</label>
+         <input
+           name='town'
+           style={formInputStyle}
+           onChange={handleInput} />
+   
+         <button style={formButtonStyle}>Order</button>
+       </form>
+      ) : (
+        <div>
+          <p>Thank you for ordering some hip dudes!</p>
+          <button onClick={() => navigate('/')}>Go back to shop</button>
+        </div>
+      )}
+     
+  </>  
   );
 }
 
