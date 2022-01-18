9e2d0031198a52325714ee3056ddf85fdcad2580
Geoblin <Oscar_niklasson94@hotmail.com>
2022-01-18 12:11:10 +0100

    cart changes

--- src/components/general/Cart.js
+++ src/components/general/Cart.js
@@ -9,16 +9,70 @@ const Cart = (props) =>  {
     const priceArray = cart.map((p) => p.price);
 
     const removeProduct = (id) => {
-    setCart([...cart].filter((product) => product.id !== id));
+    setCart([...Cart].filter((product) => product.id !== id));
   };
 
 
 
-    return(
-        <>
-            <h1>I'm a Cart</h1>
-        </>
-    );
+  return (
+    <>
+      {isCart && (
+        <div>
+          <div>
+            {cart.length <= 0 && (
+              <p>There are no products in your cart...</p>
+            )}
+            {cart.length > 0 &&
+              filteredCart.map((product) => (
+                <div
+                  key={product.id}>
+                  <p>
+                    {product.name} {product.qty} st <br />
+                    {product.price * product.qty} kr
+                  </p>
+                  <button onClick={() => removeProduct(product.id)}>
+                    Remove
+                  </button>
+                </div>
+              ))}
+          </div>
+          <p>
+            Total price:{" "}
+            {priceArray.length > 0
+              ? priceArray.reduce((total, price) => total + price)
+              : "0"}{" "}
+            kr
+          </p>
+          <button onClick={() => setCartVisible(false)}>Hide cart</button>
+        </div>
+      )}
+      {!isCart && (
+        <div>
+          {Cart.length <= 0 && <p>There are no products in your cart...</p>}
+          {Cart.length > 0 &&
+            filteredcart.map((product) => (
+              <div
+                key={product.id}>
+                <p>
+                  {product.name} {product.qty} st <br />
+                  {product.price * product.qty} kr
+                </p>
+                <button onClick={() => removeProduct(product.id)}>
+                  Remove
+                </button>
+              </div>
+            ))}
+          <p>
+            Total price:{" "}
+            {priceArray.length > 0
+              ? priceArray.reduce((total, price) => total + price)
+              : "0"}{" "}
+            kr
+          </p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Cart;
