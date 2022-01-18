b0a966c7d24ee0999c32e013865a5f71a26b86c0
Erik <43432787+ErikLager@users.noreply.github.com>
2022-01-18 14:25:43 +0100

    price in int

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -2,67 +2,67 @@ const productsService = [
   {
     id: 0,
     name: 'Stefan Löven',
-    price: '666',
+    price: 666,
   },
   {
     id: 1,
     name: 'Donald Trump',
-    price: '12',
+    price: 12,
   },
   {
     id: 2,
     name: 'Boris Johnson',
-    price: '14',
+    price: 14,
   },
   {
     id: 3,
     name: 'Vladimir Putin',
-    price: '10',
+    price: 10,
   },
   {
     id: 4,
     name: 'Silvio Berlusconi',
-    price: '20',
+    price: 20,
   },
   {
     id: 5,
     name: 'Kim Jong-un',
-    price: '23',
+    price: 23,
   },
   {
     id: 6,
     name: 'Xi Jinping',
-    price: '5',
+    price: 5,
   },
   {
     id: 7,
     name: 'Benito Mussolini',
-    price: '1',
+    price: 1,
   },
   {
     id: 8,
     name: 'Winston Churchill',
-    price: '200',
+    price: 200,
   },
   {
     id: 9,
     name: 'Neville Chamberlain',
-    price: '15',
+    price: 15,
   },
   {
     id: 10,
     name: 'Harry S. Truman',
-    price: '15',
+    price: 15,
   },
   {
     id: 11,
     name: 'George Washington',
-    price: '80',
+    price: 80,
   },
   {
     id: 12,
     name: 'Queen Elizabeth II',
-    price: '5000',
+    price: 5000,
   },
 ];
 
