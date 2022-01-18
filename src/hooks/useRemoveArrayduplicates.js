ef1b49d0bbf3de4a67445f58941ab62d4ca3582c
Geoblin <Oscar_niklasson94@hotmail.com>
2022-01-18 13:38:38 +0100

    added hook removedups

---
+++ src/hooks/useRemoveArrayduplicates.js
@@ -0,0 +1,8 @@
+const useRemoveArrayDuplicates = (array) => {
+    return array.filter(
+      (value, index, arr) => arr.findIndex((val) => value.id === val.id) === index
+    );
+  };
+  
+  export default useRemoveArrayDuplicates;
+  
