1ac5228c243d5b774f4bbdbd5ddcdc580e6fa874
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-01-18 13:53:57 +0100

    shop

--- src/components/views/Shop.js
+++ src/components/views/Shop.js
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 import ProductList from '../general/ProductList';
+import { CartContext } from '../../context/CartContext';
+import { useContext } from 'react';
 
 const Shop = () => {
+  const { cart } = useContext(CartContext);
+  
   return (
     <main>
       <Link to="/checkout">To checkout</Link>
+      <p>Items in cart: {cart.length}</p>
       <h1>Goa gubbar till salu</h1>
       <h2>Billigt billigt</h2>
       <ProductList />
