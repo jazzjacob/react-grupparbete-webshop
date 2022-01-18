30693c93a3bbf49e8436a61900ac2078b28c6f84
Geoblin <Oscar_niklasson94@hotmail.com>
2022-01-18 15:11:43 +0100

    Cartupdate

--- src/components/general/Cart.js
+++ src/components/general/Cart.js
@@ -1,41 +1,78 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import useRemoveArrayDuplicates from "../../hooks/useRemoveArrayDuplicates.js";
 
 const Cart = (props) =>  {
     const { isCart = true } = props;
 
     const { cart, setCart, setCartVisible } = useContext(CartContext);
+    const filteredCart = useRemoveArrayDuplicates(cart);
 
     const priceArray = cart.map((p) => p.price);
 
     const removeProduct = (id) => {
-    setCart([...cart].filter((product) => product.id !== id));
+    setCart([...cart].filter((prod) => prod.id !== id));
   };
-
-
-
+const CartProdstyling ={
+  border: "1px solid black",
+    textAlign: 'center',
+    marginBottom: "5px",
+    width: "200px"
+}
   return (
     <>
       {isCart && (
         <div>
           <div>
-            {Cart.length <= 0 && (
+            {cart.length <= 0 && (
               <p>There are no products in your cart...</p>
             )}
+            {cart.length > 0 &&
+              filteredCart.map((prod) => (
+                <div
+                  key={prod.id}>
+                  <p style={{border: "1px solid black",
+                            textAlign: 'center',
+                            marginBottom: "5px",
+                            width: "200px"}}>
+                    {prod.name} {prod.qty} st <br />
+                    {prod.price * prod.qty} kr
+                  <button  onClick={() => removeProduct(prod.id)}>
+                    Remove
+                  </button>
+                  </p>
+                </div>
+              ))}
           </div>
-          <p>
+          <p style={{ 
+              textAlign: 'center',
+              marginBottom: "5px",
+              width: "200px" }}>
             Total price:{" "}
             {priceArray.length > 0
               ? priceArray.reduce((total, price) => total + price)
               : "0"}{" "}
             kr
           </p>
-          <button onClick={() => setCartVisible(false)}>Hide cart</button>
+          <button onClick={() => setCartVisible(false)}>Hide Cart</button>
         </div>
       )}
       {!isCart && (
         <div>
-          {Cart.length <= 0 && <p>There are no products in your cart...</p>}
+          {cart.length <= 0 && <p>There are no products in your cart...</p>}
+          {cart.length > 0 &&
+            filteredCart.map((prod) => (
+              <div
+                key={prod.id}>
+                <p>
+                  {prod.name} {prod.qty} st <br />
+                  {prod.price * prod.qty} kr
+                </p>
+                <button onClick={() => removeProduct(prod.id)}>
+                  Remove
+                </button>
+              </div>
+            ))}
           <p>
             Total price:{" "}
             {priceArray.length > 0
