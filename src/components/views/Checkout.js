c8b3b230df47be83ae23569bc235ee19d12499a2
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-01-18 10:52:20 +0100

    updated checkout

--- src/components/views/Checkout.js
+++ src/components/views/Checkout.js
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import ShippingForm from '../general/ShippingForm';
+import LoginForm from '../general/LoginForm';
+import { useState, useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
 
-const Checkout = () =>{
-    return (
-        <>
-            <h1>This is the Checkout page</h1>
-            <Link to="/">Return to Shopping</Link>
-        </>
-    );
+const Checkout = () => {
+
+  const { loggedIn, setLoggedIn, skipLogin, setSkipLogin } = useContext(UserContext);
+  console.log(loggedIn);
+  console.log(skipLogin);
+
+  return (
+    <>
+      <h1>This is the Checkout page</h1>
+      {loggedIn || skipLogin ? <ShippingForm /> : <LoginForm />}
+    </>
+  );
 };
 
 export default Checkout;
