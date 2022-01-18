eeea3aedf13bf69bb1d4f748515389ca8d4c680e
Geoblin <Oscar_niklasson94@hotmail.com>
2022-01-18 13:20:07 +0100

    Cartprovider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import UserProvider from './context/UserContext';
+import CartProvider from './context/CartContext';
 
 ReactDOM.render(
   <UserProvider>
+    <CartProvider>
     <App />
+    </CartProvider>
   </UserProvider>,
   document.getElementById('root')
 );
