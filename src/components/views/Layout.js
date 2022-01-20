4c2ff4203c1ffe390ca393cbecbed769ea7c5d14
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-01-20 09:49:06 +0100

    removed link to navbar

--- src/components/views/Layout.js
+++ src/components/views/Layout.js
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../general/Navbar";
 
 const Layout = () => {
     return(
         <>
-            {/* <Navbar/> */}
             <Outlet/>
         </>
     );
